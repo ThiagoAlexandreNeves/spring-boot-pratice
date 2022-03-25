c34a603f1862b5799e3fef2b5e9f47a579019c2a
ThiagoAlexandreNeves <thiago,alexandre.ac@gmail.com>
2022-03-25 01:51:46 -0300

    update lombok again

--- src/main/java/com/rodrigopereiras/bookstoremanager/models/Book.java
+++ src/main/java/com/rodrigopereiras/bookstoremanager/models/Book.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Book {
 	
 	@Id
